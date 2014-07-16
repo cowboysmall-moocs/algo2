d12cf67da5d2f5cd5263e8a69ab45dea38f29ef4
Jerry Kiely <jerry@cowboysmall.com>
2014-07-16 04:29:14 -0400

    some more enhancements - still (so far unsuccessfully) trying to get it to finish in a timely manner :-)

--- src/week2/clustering3.py
+++ src/week2/clustering3.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 
 from collections import defaultdict
 from union_find import UnionFind
@@ -12,50 +13,34 @@ def construct(file_path):
 
         count = 0
         for line in file:
-            string = ''.join(line.strip().split())
-            number = int(string, 2)
-
-            if number not in vertices:
-                vertices[number] = count
-                count += 1
+            number = int(''.join(line.strip().split()), 2)
+            vertices[number] = count
+            count += 1
 
     return vertices
 
 
 
-def generate_hamming_list_integers(bit_string, distance):
-    return [int(string, 2) for string in generate_hamming_list(bit_string, distance)]
-
-
-
-def generate_hamming_list(bit_string, distance):
-    if distance == 0:
-        return [bit_string]
-    else:
-        hamming_list = []
-        for index in range(len(bit_string)):
-            if len(bit_string[index:]) > distance - 1:
-                if bit_string[index] == "0":
-                    rest_of_list = generate_hamming_list(bit_string[index + 1:], distance - 1)
-                    hamming_list.extend(map(lambda x: bit_string[:index] + "1" + x, rest_of_list))
-                else:
-                    rest_of_list = generate_hamming_list(bit_string[index + 1:], distance - 1)
-                    hamming_list.extend(map(lambda x: bit_string[:index] + "0" + x, rest_of_list))                
-        return hamming_list
+def generate_distances(bit_count, distance):
+    result = []
+    for bits in itertools.combinations(range(bit_count), distance):
+        zeros = ['0'] * bit_count
+        for bit in bits:
+            zeros[bit] = '1'
+        result.append(int(''.join(zeros), 2))
+    return result
 
 
 
 def main(argv):
-    vertices = construct(argv[0])
-
-    hamming  = generate_hamming_list_integers('000000000000000000000000', 2)
-    hamming.extend(generate_hamming_list_integers('000000000000000000000000', 1))
+    vertices  = construct(argv[0])
+    distances = generate_distances(24, 2) + generate_distances(24, 1)
 
     uf = UnionFind(len(vertices))
 
     for vertex in vertices:
-        for value in hamming:
-            candidate = vertex ^ value
+        for distance in distances:
+            candidate = vertex ^ distance
             if candidate in vertices:
                 if not uf.connected(vertices[vertex], vertices[candidate]):
                     uf.union(vertices[vertex], vertices[candidate])
