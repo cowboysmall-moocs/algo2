fbd4178f5f6ccc5eacb0903b0542c0670fa58222
Jerry Kiely <jerry@cowboysmall.com>
2014-07-23 09:46:38 -0400

    some slight changes to knapsack2.py - gave some variables more meaningful names (the names they had were meaningful, but only in the context of the lectures)

--- src/week3/knapsack2.py
+++ src/week3/knapsack2.py
@@ -22,34 +22,38 @@ def main(argv):
     W, n, items = construct(argv[0])
 
 
-    ix = defaultdict(list)
-    ix[n].append(W)
+    relevant_entries = defaultdict(list)
+    relevant_entries[n].append(W)
     for i in range(n - 1, 0, -1):
-        temp  = set(ix[i + 1] + [w - items[i - 1][0] for w in ix[i + 1]])
-        ix[i].extend([t for t in temp if t >= 0])
+        no_dupes     = set(relevant_entries[i + 1] + [w - items[i - 1][0] for w in relevant_entries[i + 1]])
+        non_negative = [entry for entry in no_dupes if entry >= 0]
+        relevant_entries[i].extend(non_negative)
 
 
-    entries = []
-    for key, values in ix.iteritems():
+    relevant_entries_list = []
+    for key, values in relevant_entries.iteritems():
         for value in values:
-            entries.append((key, value))
+            relevant_entries_list.append((key, value))
 
 
     A = defaultdict(int)
-    for entry in entries:
+    for entry in relevant_entries_list:
+        left_entry = (entry[0] - 1, entry[1])
         if items[entry[0] - 1][0] <= entry[1]:
-            A[entry] = max( A[ (entry[0] - 1, entry[1]) ], A[ (entry[0] - 1, entry[1] - items[entry[0] - 1][0]) ] + items[entry[0] - 1][1])
+            current_weight = items[entry[0] - 1][0]
+            current_value  = items[entry[0] - 1][1]
+            previous_entry = (entry[0] - 1, entry[1] - current_weight)
+            A[ entry ] = max( A[ left_entry ], A[ previous_entry ] + current_value)
         else:
-            A[entry] = A[ (entry[0] - 1, entry[1]) ]
-
+            A[ entry ] = A[ left_entry ]
 
 
     print
-    print 'W x N     = ', (W * n)
-    print '|entries| = ', len(entries)
-    print '%         = ', (float(len(entries)) * 100) / (W * n)  
-    print '|A|       = ', len(A)
-    print 'Value     = ', A[(n, W)]
+    print '                Dimensions of problem (n x W): ', (W * n)
+    print '                    Count of relevant entries: ', len(relevant_entries_list)
+    print 'Percentage of relevant entries to all entries: ', (float(len(relevant_entries_list)) * 100) / (W * n)  
+    print '            Count of calculated entries (|A|): ', len(A)
+    print '                                    A[(n, W)]: ', A[(n, W)]
     print
 
 
