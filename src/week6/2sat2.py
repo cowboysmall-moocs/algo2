31d0adfe15dbe54ebc0ec0821ac0da61b6852112
Jerry Kiely <jerry@cowboysmall.com>
2014-08-11 04:02:31 -0400

    first attempt at 2sat (Papadimitriou version)

--- src/week6/2sat2.py
+++ src/week6/2sat2.py
@@ -1,4 +1,9 @@
 import sys
+import math
+import random
+
+from collections import defaultdict
+from itertools import chain
 
 
 def construct(file_path):
@@ -14,11 +19,53 @@ def construct(file_path):
     return clauses
 
 
+def satisfiable(clauses):
+    n = len(clauses)
+
+    for i in range(int(math.log(n, 2))):
+
+        assignment = [random.choice([True, False]) for _ in range(n)]
+        for j in range(2 * pow(n, 2)):
+            unsatisfied_clauses = unsatisfied(clauses, assignment)
+            if not unsatisfied_clauses:
+                return True
+            else:
+                flip_index = abs(random.choice(unsatisfied_clauses)[random.randint(0, 1)]) - 1
+                assignment[flip_index] = not assignment[flip_index]
+
+    return False
+
+
+def unsatisfied(clauses, assignment):
+    unsatisfied_clauses = []
+    for clause in clauses:
+        clause_1 = (clause[0] < 0 and assignment[abs(clause[0]) - 1]) or (clause[0] > 0 and not assignment[abs(clause[0]) - 1])
+        clause_2 = (clause[1] < 0 and assignment[abs(clause[1]) - 1]) or (clause[1] > 0 and not assignment[abs(clause[1]) - 1])
+        if clause_1 and clause_2:
+            unsatisfied_clauses.append(clause)
+    return unsatisfied_clauses
+
+
+def preprocess(clauses):
+    values_dict  = defaultdict(set)
+
+    for clause in clauses:
+        values_dict[clause[0]].add(clause)
+        values_dict[clause[1]].add(clause)
+
+    reduced = set()
+    for key, values in values_dict.iteritems():
+        if -key in values_dict:
+            reduced |= values
+
+    return list(reduced)
+
 
 def main(argv):
     clauses = construct(argv[0])
 
-    if satisfiable(components):
+    if satisfiable(preprocess(clauses)):
+    # if satisfiable(clauses):
         print
         print 'Satisfiable'
         print
