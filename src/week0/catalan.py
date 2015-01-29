f28212b17bdcd9f3c8e4bc9255522d4ff217335d
Jerry Kiely <jerry@cowboysmall.com>
2015-01-28 21:13:53 -0500

    slight change to catalan number generation script

--- src/week0/catalan.py
+++ src/week0/catalan.py
@@ -4,19 +4,20 @@ import sys
 CATALAN    = {0: 1, 1: 1}
 
 def catalan(n):
-    if n in CATALAN:
-        return CATALAN[n]
-    else:
+    if n not in CATALAN:
         c = 0
 
         for i in xrange(1, n + 1):
             c += catalan(i - 1) * catalan(n - i)
 
         CATALAN[n] = c
-        return c
+
+    return CATALAN[n]
 
 
 def main(argv):
+    sys.setrecursionlimit(1048576)
+
     print catalan(int(argv[0]))
 
 
